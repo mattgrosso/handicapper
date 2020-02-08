8e2801408204c066f4e12b41eeb1771444afa5a7
Matt Grosso <mattgrosso@gmail.com>
2020-02-08 01:29:09 -0500

    Makes eslint less annoying

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: [
     // add more generic rulesets here, such as:
-    // 'eslint:recommended',
-    'plugin:vue/recommended'
+    'eslint:recommended',
+    // 'plugin:vue/recommended'
   ],
   rules: {
     // override/add rules settings here, such as:
